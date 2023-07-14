b6ec257607fba19760c2c6ed18fe71b4eb286e21
Finbyz <info@finbyz.tech>
2023-07-14 13:21:59 +0530

    patches added

---
+++ portfolio/create_doc/create_document.py
@@ -0,0 +1,25 @@
+import frappe
+
+def execute():
+    create_document()
+    
+def create_document():
+    if not frappe.db.exists("Investment Segment" , "Mutual Funds"):
+        ivs=frappe.new_doc("Investment Segment")
+        ivs.segment="Mutual Funds"
+        ivs.save()
+
+    if not frappe.db.exists("Investment Segment" , "Equity"):
+        ivs=frappe.new_doc("Investment Segment")
+        ivs.segment="Equity"
+        ivs.save()
+ 
+    if not frappe.db.exists("Category" , "Debt"):
+        cat=frappe.new_doc("Category")
+        cat.category="Debt"
+        cat.save()
+    
+    if not frappe.db.exists("Category" , "Equity"):
+        cat=frappe.new_doc("Category")
+        cat.category="Equity"
+        cat.save()
